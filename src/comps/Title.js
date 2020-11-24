f571c9b901c88d08e771164ca4c828051e294fa0
BenMekkiMouad <73074210+BenMekkiMouad@users.noreply.github.com>
2020-11-24 16:31:38 +0100

    confirmed

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -7,7 +7,7 @@ const Title = () => {
       <h2>Chehiwat Oum Salma</h2>
       <ul>
         <li><a href="https://www.facebook.com/rachida.helali.3">facebook</a></li>
-        <li><a>0622555443</a></li>
+        <li><a href="tel:+212622555443">0622555443</a></li>
       </ul>
       <p>Bienvenue chez Chehiwat Oum Salma.</p>
     </div>
