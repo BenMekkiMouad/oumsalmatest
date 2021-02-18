bdea05550355264c71dc31ce79f4b34d6794c652
BenMekkiMouad <73074210+BenMekkiMouad@users.noreply.github.com>
2021-02-18 03:42:25 +0100

    change

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -6,8 +6,8 @@ const Title = () => {
       <h1>Oum Salma</h1>
       <h2>Chehiwat Oum Salma</h2>
       <ul>
-        <li><a href="https://www.facebook.com/rachida.helali.3">facebook</a></li>
-        <li><a href="tel:+212622555443">0622555443</a></li>
+        <button><a href="https://www.facebook.com/rachida.helali.3">facebook</a></button>
+        <button><a href="tel:+212622555443">0622555443</a></button>
       </ul>
       <p>Bienvenue chez Chehiwat Oum Salma.</p>
     </div>
